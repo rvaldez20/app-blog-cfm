2664cc2b65c2db26c2dc7f697160cacf91de4ad4
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-27 14:10:25 -0600

    Se muestran los datos de la mini DB (posts) y se crean enlaces para para cada post

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import{Link} from 'react-router-dom';
+
+import posts from '../data/post';
 
 const Blog = () => {
+  
    return ( 
       <div>
          <h2> Blog</h2>
          <ul>
-            <li>Articulo #1</li>
-            <li>Articulo #2</li>
-            <li>Articulo #3</li>
+            {posts.map( post => {
+               return (
+                  <li key={post.id}>
+                     <Link to={`/post/${post.id}`} >{post.titulo}</Link>
+                  </li>
+               )
+            })}
          </ul>
       </div>   
     );
