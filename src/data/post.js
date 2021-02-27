c939a23decd63349a4709951c8ce09b8072917f8
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-27 14:09:34 -0600

    Se crear una mini Base de datos para ejemplificar el llamado a una API y mostrar los datos

---
+++ src/data/post.js
@@ -0,0 +1,7 @@
+const posts = [
+   { id: 1, titulo:'Articulo #1', texto: 'Este es el texto del primer articulo'},
+   { id: 2, titulo:'Articulo #2', texto: 'Este es el texto del segundo articulo'},
+   { id: 3, titulo:'Articulo #3', texto: 'Este es el texto del tercero articulo'}
+];
+
+export default posts;
