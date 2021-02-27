ed1c418367f187d8bd22c97250893730904b1921
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-27 13:11:00 -0600

    Se configura router-react-dom para se visualice cada componente como si fuera una pagina diferente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import{BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Header from './components/Header';
+import Inicio from './components/Inicio';
+import Blog from './components/Blog';
+import AcercaDe from './components/AcercaDe';
+
+const App = () => {
+
+
+
+  return ( 
+
+	<BrowserRouter>
+	
+		<div>
+			{/* Componente Header */}			
+			<Header />
+
+			<main>
+			
+				{/* cuando es de una linea se puede hacer asi)*/}	
+				<Route path="/" exact={true} component={Inicio} />				
+				<Route path="/blog" component={Blog} />
+				<Route path="/acerca-de" component={AcercaDe} />
+
+				{/* O s epuede dejar de varia lineas */}	
+				{/*
+				<Route path="/acerca-de">
+					<AcercaDe />     
+				</Route>
+				*/}
+
+			</main>
+		</div>  
+
+	</BrowserRouter>
+
   );
 }
-
+ 
 export default App;
